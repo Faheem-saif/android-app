fd4dd0c6e40840c1d3a7edcd348f3acf916a2ec5
Faheem-saif <faheem000saif@gmail.com>
2024-02-29 22:56:56 +0500

    Home screen

--- src/theme/Homescreen.tsx
+++ src/theme/Homescreen.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useStore} from '../store/store';
 
 const Homescreen = () => {
   const CoffeeList = useStore((state: any) => state.CoffeeList);
+  const BeanList = useStore((state:any)=> state.BeanList)
+  const [categories,setCategories]=useState([undefined])
+  const [searchText,setSearchText]=useState(undefined)
 
+  const [categorieٰIndex,setCategorieIndex]=useState({
+    index:  0,
+    category:categories[0],
+  })
+  console.log(BeanList);
+  
 
   return (
     <View>
-      <Text>Homescreen</Text>
+      <Text style={{color:"orange", padding:20}}>Homescreen</Text>
+      {BeanList.map(item =>  <Text style={{color:"orange", padding:20}}>{`${item.id}-${item.name}`}</Text> )}
     </View>
   );
 };
